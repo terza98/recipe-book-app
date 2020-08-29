f6cccaf6f94a6596829ddfb2340074e9079ec9c7
Mladen Terzic <terza98@hotmail.rs>
2020-08-29 21:28:40 +0200

    Commented autocomplete call not to  waste points from Free Api calls

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,86 +1,102 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 //bootstrap components
-import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
 
 //api
-import Service from '../api/auth-service.js';
+import ApiClient from '../api/auth-service.js';
 
 //componets
 import SearchResult from './SearchResult.js';
 
-export default function Search(props){
-    const [query, changeQuery] = useState("");
-    const [error, setError] = useState("");
+export default function Search(props) {
+	const [query, changeQuery] = useState('');
+	const [error, setError] = useState('');
 
-    const [predictions, setPredictions] = useState([]);
+	const [predictions, setPredictions] = useState([]);
 
-    const [recipeList, setRecipes] = useState([]);
-    const [showRecipes, isRecipes] = useState(false);
+	const [recipeList, setRecipes] = useState([]);
+	const [showRecipes, isRecipes] = useState(false);
 
-    useEffect(() => {
-            
-    }, [] );
+	useEffect(() => {}, []);
 
-    const handleInputChange = (e) => {
-        changeQuery(e.target.value);
+	const handleInputChange = e => {
+		changeQuery(e.target.value);
 
-        Service.autoCompleteSearch(e.target.value)
-            .then(
-                (result) => {
-                    setPredictions(result.data);
-                },
-                // Note: hanling errors here
-                (error) => {
-                    setError(error.message);
-                }
-            )
-    }
-    const selectQuery = (title) => {
-        changeQuery(title);
-        setPredictions([]);
-        console.log(title);
-    }
-    const loadResults = () => {
-        Service.searchRecipes(query)
-        .then(
-            (result) => {
-                console.log(result.data.results);
-                setRecipes(result.data.results);
-                isRecipes(true);
-            },
-            // Note: hanling errors here
-            (error) => {
-                setError(error.message);
-            }
-        )
-    }
-    return(
-        <>
-            {!showRecipes? 
-            <Container style={{margin: '10%'}} className="text-center">
-                <h1>Welcome to my Recipes app!</h1>
-                <p>Type your search inquiry below</p>
-                {error &&
-                    <Alert variant="danger" onClose={() => setError("")} dismissible>
-                        {typeof error !== "object"? error: ''}
-                    </Alert>
-                }
-                <div id="autocomplete-list">
-                    <input onChange={(e) => handleInputChange(e)} value={query} type="text" id="search" name="search"/>
-                    <ListGroup>
-                        {predictions!==undefined && predictions.map( (item,index) =>
-                            <ListGroup.Item onClick={() => selectQuery(item.title)} key={index}>{item.title}</ListGroup.Item>
-                        )}
-                    </ListGroup>
-                </div>
-                <button id="search-btn" onClick={loadResults}>Search</button>
-            </Container>
-            :
-                <SearchResult query={query} recipes={recipeList}/>
-            }
-        </>
-    )
+		//uncoment when start giving solution
+		// ApiClient.autoCompleteSearch(e.target.value)
+		// 	.then(result => {
+		// 		setPredictions(result.data);
+		// 	})
+		// 	// Note: hanling errors here
+		// 	.catch(error => {
+		// 		setError(error.message);
+		// 	});
+	};
+	const selectQuery = title => {
+		changeQuery(title);
+		setPredictions([]);
+	};
+	const loadResults = () => {
+		ApiClient.searchRecipes(query).then(
+			result => {
+				console.log(result.data.results);
+				setRecipes(result.data.results);
+				isRecipes(true);
+			},
+			// Note: hanling errors here
+			error => {
+				setError(error.message);
+			},
+		);
+	};
+	return (
+		<>
+			{!showRecipes ? (
+				<Jumbotron>
+					<h1>Welcome to my Recipes app!</h1>
+					<p>Type your search inquiry below</p>
+					{error && (
+						<Alert
+							variant="danger"
+							onClose={() => setError('')}
+							dismissible
+						>
+							{typeof error !== 'object' ? error : ''}
+						</Alert>
+					)}
+
+					<div id="autocomplete-list">
+						<input
+							onChange={e => handleInputChange(e)}
+							value={query}
+							type="text"
+							placeholder="Type in your next meal..."
+							id="search"
+							name="search"
+						/>
+						<ListGroup>
+							{predictions !== undefined &&
+								predictions.map((item, index) => (
+									<ListGroup.Item
+										onClick={() => selectQuery(item.title)}
+										key={index}
+									>
+										{item.title}
+									</ListGroup.Item>
+								))}
+						</ListGroup>
+					</div>
+					<Button id="search-btn" onClick={loadResults}>
+						Search
+					</Button>
+				</Jumbotron>
+			) : (
+				<SearchResult query={query} recipes={recipeList} />
+			)}
+		</>
+	);
 }
