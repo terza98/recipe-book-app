5a6677c4297c7a3c53a1145c28579ee749c8fe59
Mladen Terzic <terza98@hotmail.rs>
2020-08-29 21:26:43 +0200

    Updated Header with Routes to Favourites

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,34 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 //components
-import Favourites from './Favourites.js'
-import Search from './Search.js'
+import Search from './Search.js';
+import SearchResult from './SearchResult.js';
 
-export default function Header(props){
-    return(
-        <Router>
-            <Navbar id="navbar" bg="dark" variant="dark">
-                <Navbar.Brand href="/">
-                    {props.title}
-                </Navbar.Brand>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Link to="/search">Search recipes</Link>
-                        <Link to="/favourite">Favourite recipes</Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            <Switch>
-                <Route path="/">
-                    <Search />
-                </Route>
-                <Route path="/search">
-                    <Search />
-                </Route>
-                <Route path="/favourites">
-                    <Favourites />
-                </Route>
-            </Switch>
-        </Router>
-    );
+export default function Header(props) {
+	return (
+		<Router>
+			<Navbar id="navbar" bg="dark" variant="dark">
+				<Navbar.Brand href="/">{props.title}</Navbar.Brand>
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="ml-auto">
+						<Link to="/search">Search</Link>
+						<Link to="/favourites">Favourites</Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			<Switch>
+				<Route path="/search">
+					<Search />
+				</Route>
+				<Route path="/favourites">
+					<SearchResult
+						recipes={JSON.parse(localStorage.getItem('favourites'))}
+					/>
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
